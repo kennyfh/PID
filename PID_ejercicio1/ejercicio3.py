d6a97c6d06d2ec2251ab5702b68e1fab3ffa907c
Kenny F <kennyjesus@protonmail.com>
2020-10-17 15:35:36 +0200

    Añadir ejercicio1

---
+++ PID_ejercicio1/ejercicio3.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+"""
+Cargados imágenes en escala de grises de 256 niveles de gris.
+Realiza una “superposición” de ambas imágenes sumándolas.
+ Al sumar los niveles de gris, teóricamente se obtendrían valores entre 0 y 510. Averiguacómo está tratando el software usado el rango de valores de salida. Es decir, ¿se han truncado todos los valores mayores de 255? ¿Se han rescalado? A la vista de las respuestas anteriores ¿se ha perdido mucha información? Si es así, ¿cómo se podría mejorar el resultado?
+
+"""
+img = cv2.imread('ejemplo2.jpg',0) #1280x1920
+img = cv2.resize(img, (1920,1080))
+img2 = cv2.imread('ejemplo.jpg',0) #1080x1920
+
+cv2.imshow('foto1', img)
+cv2.waitKey(0)
+cv2.imshow('foto2', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+superposicion = np.add(img,img2) ## Por el principio de adición de matrices, ambas matrices tienen que tener misma dimensión
+superposicion = superposicion.astype(np.uint8)
+print(superposicion)
+# cv2.imshow('superposicion', superposicion)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
